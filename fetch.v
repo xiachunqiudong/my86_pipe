2ab0685e4a8133764bc060b394c5f2dae17b2195
xiachunqiudong <xiadaoliang@outlook.com>
2022-10-20 10:22:58 +0800

    2022/10/20

--- fetch.v
+++ fetch.v
@@ -1,12 +1,24 @@
+`include "define.v"
+
 module fetch(
-    input  wire [`D_WORD] F_pred_pc_i,
-    output reg  [`NIBBLE] f_icode_o,
-    output reg  [`NIBBLE] f_ifun_o,
-    output reg  [`NIBBLE] f_rA_o,
-    output reg  [`NIBBLE] f_rB_o,
-    output reg [`D_WORD]  f_valC_o,
+    // input
+    input wire [`D_WORD] F_pred_pc_i,
+    // 访存阶段 分支预测错误
+    input wire [`NIBBLE] M_icode_i,
+    input wire [`D_WORD] M_valA_i,
+    input wire           M_Cnd_i,
+    // 写回阶段 得到ret返回地址
+    input wire [`NIBBLE] W_icode_i,
+    input wire [`D_WORD] W_valM_i,
+    // output
+    output reg [`NIBBLE] f_icode_o,
+    output reg [`NIBBLE] f_ifun_o,
+    output reg [`NIBBLE] f_rA_o,
+    output reg [`NIBBLE] f_rB_o,
+    output reg [`D_WORD] f_valC_o,
     output reg [`D_WORD] f_valP_o,
-    output reg [`D_WORD] f_pred_pc_o
+    // 流水线cpu在取指阶段就需要计算出下一条指令地址
+    output reg [`D_WORD] f_pred_pc_o,
     output reg [`NIBBLE] f_stat_o
 );
 
@@ -15,6 +27,31 @@ module fetch(
     reg [79:0] instr;
     reg [`BYTE] instr_mem [0:1023];
 
+    assign imem_error  = (f_pc > 1023);
+    assign instr_valid = (f_icode_o <= 4'hb);
+
+    // f_stat_o
+    always@(*) begin
+        if(imem_error)
+            f_stat_o = `SADR;
+        else if(~instr_valid)
+            f_stat_o = `SINS;
+        else if(f_icode_o == `IHALT)
+            f_stat_o = `SHLT;
+        else
+            f_stat_o = `SAOK;         
+    end
+
+    // f_pc
+    always@(*) begin
+        if(M_icode_i == `IJXX && M_Cnd_i == 0) // 分支预测错误 并没有跳转
+            f_pc = M_valA_i;
+        else if(W_icode_i == `IRET) // 返回指令
+            f_pc = W_valM_i;
+        else
+            f_pc = F_pred_pc_i;
+    end
+
     // 根据 f_pc 从指令内存中取指令
     integer i;
     always@(*) begin
@@ -22,13 +59,18 @@ module fetch(
             instr[(8*(10 - i) - 1) -: 8] = instr_mem[f_pc + i];
     end
 
-
     // split
     always@(*) begin
         f_icode_o = instr[79:76];
-        f_ifun    = instr[75:72];
+        f_ifun_o  = instr[75:72];
     end
 
+    assign need_reg  = f_icode_o == `IIRMOVQ || f_icode_o == `IMRMOVQ || f_icode_o == `IRMMOVQ 
+                    || f_icode_o == `ICMOVQ  || f_icode_o == `IOPQ    || f_icode_o == `IPUSHQ  
+                    || f_icode_o == `IPOPQ;
+
+    assign need_valC = f_icode_o == `IIRMOVQ || f_icode_o == `IMRMOVQ || f_icode_o == `IRMMOVQ
+                    || f_icode_o == `ICALL   || f_icode_o == `IJXX;
     
     // align
     always@(*) begin
@@ -43,15 +85,21 @@ module fetch(
             f_valC_o = instr[71:8];
         end
     end
+
+    //assign f_valP_o = f_pc + 1 + need_reg + (need_valC);
+    always@(*) begin
+        f_valP_o = f_pc + 1 + need_reg;
+        if(need_valC)
+            f_valP_o = f_valP_o + 8;
+    end
+
+    // predict pc
+    // 分支预测JXX总是跳转
+    always@(*) begin
+        if(f_icode_o == `ICALL || f_icode_o == `IJXX)
+            f_pred_pc_o = f_valC_o;
+        else
+            f_pred_pc_o = f_valP_o;
+    end
     
-    
-
-    assign imem_error  = (f_pc > 1023);
-    assign instr_valid = (f_icode_o <= 4'hb);
-
-
-    // instr mem
-    
-
-
 endmodule
--- fetch_reg.v
+++ fetch_reg.v
@@ -1,16 +1,21 @@
+`include "define.v"
+
 module fetch_reg(
-    input wire clk_i,
-    input wire rstn_i,
-    input wire [`D_WORD] pred_pc_i,
-    output reg [`D_WORD] f_pred_pc_o
+    input  wire clk_i,
+    input  wire rstn_i,
+    input  wire [`D_WORD] f_pred_pc_i,
+    output wire [`D_WORD] F_pred_pc_o
 );
 
-    always@(posedge clk) begin
-        if(~rstn_i)
-            f_pred_pc_o <= 0;
-        else
-            f_pred_pc_o <= pred_pc_i;
-    end
+    reg [`D_WORD] pred_pc_reg;
 
+    assign f_pred_pc_o = pred_pc_reg;
+
+    always@(posedge clk_i) begin
+        if(~rstn_i)
+            pred_pc_reg <= 0;
+        else
+            pred_pc_reg <= f_pred_pc_i;
+    end
 
 endmodule
